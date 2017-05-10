3a7e2ee89428e8776e55d693c932f1c4a682f2b2
swainn <nathan.swain86@gmail.com>
2017-05-09 21:08:29 -0500

    Solution to intermediate section of tutorial.

--- tethysapp/dam_inventory/controllers.py
+++ tethysapp/dam_inventory/controllers.py
@@ -1,7 +1,10 @@
 from django.core.urlresolvers import reverse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from tethys_sdk.gizmos import MapView, Button
+
+from tethys_sdk.gizmos import MapView, Button, TextInput, DatePicker, SelectInput
+from .model import add_new_dam, get_all_dams
 
 
 @login_required()
@@ -9,14 +12,12 @@ def home(request):
     """
     Controller for the app home page.
     """
-
     dam_inventory_map = MapView(
         height='100%',
         width='100%',
         layers=[],
         basemap='OpenStreetMap',
     )
-
 
     add_dam_button = Button(
         display_text='Add Dam',
@@ -39,11 +40,89 @@ def add_dam(request):
     """
     Controller for the Add Dam page.
     """
+    # Default Values
+    name = ''
+    owner = 'reclamation'
+    river = ''
+    date_built = ''
+
+    # Errors
+    name_error = ''
+    owner_error = ''
+    river_error = ''
+    date_error = ''
+
+    # Handle form submission
+    if request.POST and 'add-button' in request.POST:
+        # Get values
+        has_errors = False
+        name = request.POST.get('name', None)
+        owner = request.POST.get('owner', None)
+        river = request.POST.get('river', None)
+        date_built = request.POST.get('date-built', None)
+
+        # Validate
+        if not name:
+            has_errors = True
+            name_error = 'Name is required.'
+
+        if not river:
+            has_errors = True
+            river_error = 'Required.'
+
+        if not date_built:
+            has_errors = True
+            date_error = 'Required.'
+
+        if not has_errors:
+            add_new_dam(name=name, owner=owner, river=river, date_built=date_built)
+            return redirect(reverse('dam_inventory:home'))
+
+        messages.error(request, "Please fix errors.")
+
+    # Define form gizmos
+    name_input = TextInput(
+        display_text='Name',
+        name='name',
+        initial=name,
+        error=name_error
+    )
+
+    owner_input = SelectInput(
+        display_text='Owner',
+        name='owner',
+        multiple=False,
+        options=[('Reclamation', 'reclamation'), ('Army Corp', 'armycorp'), ('Other', 'other')],
+        initial=owner,
+        error=owner_error
+    )
+
+    river_input = TextInput(
+        display_text='River',
+        name='river',
+        placeholder='e.g.: Mississippi River',
+        initial=river,
+        error=river_error
+    )
+
+    date_built = DatePicker(
+        name='date-built',
+        display_text='Date Built',
+        autoclose=True,
+        format='MM d, yyyy',
+        start_view='decade',
+        today_button=True,
+        initial=date_built,
+        error=date_error
+    )
+
     add_button = Button(
         display_text='Add',
         name='add-button',
         icon='glyphicon glyphicon-plus',
         style='success',
+        attributes={'form': 'add-dam-form'},
+        submit=True
     )
 
     cancel_button = Button(
@@ -53,8 +132,23 @@ def add_dam(request):
     )
 
     context = {
+        'name_input': name_input,
+        'owner_input': owner_input,
+        'river_input': river_input,
+        'date_built_input': date_built,
         'add_button': add_button,
         'cancel_button': cancel_button,
     }
 
     return render(request, 'dam_inventory/add_dam.html', context)
+
+
+@login_required()
+def list_dams(request):
+    """
+    Show all dams in a table view.
+    """
+    dams = get_all_dams()
+    print(dams)
+    context = {'dams': dams}
+    return render(request, 'dam_inventory/list_dams.html', context)
