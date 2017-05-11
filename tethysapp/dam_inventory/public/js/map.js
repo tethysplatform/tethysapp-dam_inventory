a0464a35ed19c63a372c4570208ab543ee677be0
swainn <nathan.swain86@gmail.com>
2017-05-10 23:49:24 -0500

    Solution to advanced solution section of tutorial.

--- tethysapp/dam_inventory/public/js/map.js
+++ tethysapp/dam_inventory/public/js/map.js
@@ -1,46 +1,63 @@
-$(function() {
+$(function()
+{
+    // Create new Overlay with the #popup element
     var popup = new ol.Overlay({
         element: document.getElementById('popup')
     });
 
+    // Get the Open Layers map object from the Tethys MapView
     var map = TETHYS_MAP_VIEW.getMap();
 
-    // Get the Select Interaction
+    // Get the Select Interaction from the Tethys MapView
     var select_interaction = TETHYS_MAP_VIEW.getSelectInteraction();
 
-    // Add Overlay to map
+    // Add the popup overlay to the map
     map.addOverlay(popup);
 
     // When selected, call function to display properties
-    select_interaction.getFeatures().on('change:length', function(e) {
+    select_interaction.getFeatures().on('change:length', function(e)
+    {
         var popup_element = popup.getElement();
 
-        if (e.target.getArray().length > 0) {
+        if (e.target.getArray().length > 0)
+        {
             // this means there is at least 1 feature selected
             var selected_feature = e.target.item(0); // 1st feature in Collection
-            var coordinate = selected_feature.getGeometry().getCoordinates();
+
+            // Get coordinates of the point to set position of the popup
+            var coordinates = selected_feature.getGeometry().getCoordinates();
+
+            var popup_content = '<div class="dam-popup">' +
+                                    '<p><b>' + selected_feature.get('name') + '</b></p>' +
+                                    '<table class="table  table-condensed">' +
+                                        '<tr>' +
+                                            '<th>Owner:</th>' +
+                                            '<td>' + selected_feature.get('owner') + '</td>' +
+                                        '</tr>' +
+                                        '<tr>' +
+                                            '<th>River:</th>' +
+                                            '<td>' + selected_feature.get('river') + '</td>' +
+                                        '</tr>' +
+                                        '<tr>' +
+                                            '<th>Date Built:</th>' +
+                                            '<td>' + selected_feature.get('date_built') + '</td>' +
+                                        '</tr>' +
+                                    '</table>' +
+                                '</div>';
 
             // Clean up last popup and reinitialize
             $(popup_element).popover('destroy');
-            popup.setPosition(coordinate);
+            popup.setPosition(coordinates);
 
             $(popup_element).popover({
               'placement': 'top',
-              'animation': false,
+              'animation': true,
               'html': true,
-              'content': '<p>Foo bar:</p>'
+              'content': popup_content
             });
             $(popup_element).popover('show');
-
-
-            console.log(selected_feature);
-            console.log(selected_feature.get('name'));
-            console.log(selected_feature.get('owner'));
-            console.log(selected_feature.get('river'));
-            console.log(selected_feature.get('date_built'));
-
         } else {
-            // remove pop up.
+            // remove pop up when selecting nothing on the map
             $(popup_element).popover('destroy');
         }
     });
